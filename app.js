8314ea26338c5989662f41846fbf81b045a8de12
Mustago <muga200301@gmail.com>
2023-04-20 07:56:34 +0500

    full

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-let arr = [1, 2, 3, "hi"];
-// удалить элементи от индекси 1 до 3
-let newArr = arr.splice(1, 3);
-console.log(arr);
 
+console.log(sum);
 
-let arr2 = [2, 3, 4, 7, 8, 9];
-// добавить элементи от индекси 2 и не удаляет не каких ( 0) и добавить "1", true, 25
-let newArr2 = arr2.splice(2, 0, "1", true, 25);
-console.log(arr2);
-
-
+// элементхои массивро строка мекунем
 
 // let arr3 = [2, 3, 4, 7, 8, 9];
 // // вырезать элементи последный
 // let newArr3 = arr.slice(-1);
 // console.log(newArr3);
-
 
 // let arr2 = [1, 2, 3, "hi"];
 // // если не найдот то возвращает false
